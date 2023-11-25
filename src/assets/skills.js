2c4bc046992bdb55694e988ac8fe8f18bef618a2
vikiru <72267229+vikiru@users.noreply.github.com>
2023-11-25 15:29:41 -0500

    Update Skills section

--- src/assets/skills.js
+++ src/assets/skills.js
@@ -1,8 +1,24 @@
 const skillsData = {
-  languages: ["Java", "Python", "HTML", "CSS", "JavaScript"],
-  frameworks: [],
-  libraries: [],
-  devops: [],
-  tools: [],
-  concepts: [],
+  languages: ["Java", "Python", "HTML", "CSS", "JavaScript", "Markdown"],
+  runtime: ["Nodejs"],
+  frameworks: ["Spring", "JUnit", "Mocha", "Django", "Flask", "React"],
+  libraries: ["NumPy", "SciPy", "SimPy", "Pandas", "Chai"],
+  database: ["MySQL", "SQLite", "MongoDB", "Sequelize"],
+  devops: ["CircleCI", "Github Actions", "Snyk"],
+  tools: [
+    "Maven",
+    "NPM",
+    "pip",
+    "Git",
+    "GitHub",
+    "GitHub Desktop",
+    "Postman",
+    "ESLint",
+    "Prettier",
+  ],
+  ide: ["IntelliJ", "Eclipse"],
+  editor: ["VSCode"],
+  hosting: ["GitHub Pages", "Heroku", "Render"],
 };
+
+export default skillsData;
