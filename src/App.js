4662a03cb5f79b037f84c70ebc84b9e2018a18f9
vikiru <72267229+vikiru@users.noreply.github.com>
2023-11-25 18:01:28 -0500

    Update main App

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Footer from "./components/Footer/Footer";
 import NavBar from "./components/NavBar/NavBar";
 import ProjectContainer from "./components/ProjectContainer/ProjectContainer";
 import React from "react";
+import SkillsContainer from "./components/SkillsContainer/SkillsContainer";
 
 function App() {
   return (
     <div className="App h-screen w-screen overflow-x-hidden bg-black">
       <NavBar />
+      <SkillsContainer />
       <ProjectContainer />
       <Footer />
     </div>
