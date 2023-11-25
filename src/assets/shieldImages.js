c57b9c9c848386497b9e360ac04d29d36ad763fd
vikiru <72267229+vikiru@users.noreply.github.com>
2023-11-25 10:38:27 -0500

    Create shieldImages.js

---
+++ src/assets/shieldImages.js
@@ -0,0 +1,42 @@
+const shieldImages = {
+  Java: "https://img.shields.io/badge/java-%23ED8B00.svg?style=for-the-badge&logo=openjdk&logoColor=white",
+  Python:
+    "https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54",
+  HTML: "https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white",
+  CSS: "https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white",
+  JavaScript:
+    "https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E",
+  Spring:
+    "https://img.shields.io/badge/Spring-6DB33F?style=for-the-badge&logo=spring&logoColor=white",
+  Thymeleaf:
+    "https://img.shields.io/badge/Thymeleaf-%23005C0F.svg?style=for-the-badge&logo=Thymeleaf&logoColor=white",
+  JUnit:
+    "https://img.shields.io/badge/Junit5-25A162?style=for-the-badge&logo=junit5&logoColor=white",
+  Nodejs:
+    "https://img.shields.io/badge/node.js-6DA55F?style=for-the-badge&logo=node.js&logoColor=white",
+  Mocha:
+    "https://img.shields.io/badge/-mocha-%238D6748?style=for-the-badge&logo=mocha&logoColor=white",
+  Chai: "https://img.shields.io/badge/chai-A30701?style=for-the-badge&logo=chai&logoColor=white",
+  NumPy:
+    "https://img.shields.io/badge/Numpy-777BB4?style=for-the-badge&logo=numpy&logoColor=white",
+  SciPy:
+    "https://img.shields.io/badge/SciPy-654FF0?style=for-the-badge&logo=SciPy&logoColor=white",
+  SimPy:
+    "https://img.shields.io/badge/SimPy-3776AB?style=for-the-badge&logoColor=white",
+  SQLite:
+    "https://img.shields.io/badge/sqlite-%2307405e.svg?style=for-the-badge&logo=sqlite&logoColor=white",
+  Sequelize:
+    "https://img.shields.io/badge/Sequelize-52B0E7?style=for-the-badge&logo=Sequelize&logoColor=white",
+  Maven:
+    "https://img.shields.io/badge/apache_maven-C71A36?style=for-the-badge&logo=apachemaven&logoColor=white",
+  Swing:
+    "https://img.shields.io/badge/java%20swing-%23ED8B00.svg?style=for-the-badge&logo=openjdk&logoColor=white",
+  UDP: "",
+  Threads: "",
+  JavaFX:
+    "https://img.shields.io/badge/java%20fx-%23ED8B00.svg?style=for-the-badge&logo=openjdk&logoColor=white",
+  discordjs:
+    "https://img.shields.io/badge/discord.js-7289DA?style=for-the-badge&logo=discordjs&logoColor=white",
+};
+
+export default shieldImages;
