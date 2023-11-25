210380c066de3646079129e38924e4e5d6aa72a5
vikiru <72267229+vikiru@users.noreply.github.com>
2023-11-25 18:39:01 -0500

    Update footer with copyright statement

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,24 +4,34 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
 function Footer() {
+  const currentYear = new Date().getFullYear();
   return (
-    <footer id="contact">
-      <p className="pb-2 text-lg uppercase text-white">Connect with me</p>
-      <div>
-        <a href="https://github.com/vikiru">
-          <FontAwesomeIcon
-            icon={faGithub}
-            className="ease-in-out-anim pr-2 text-3xl text-white"
-          />
-        </a>
-        <a href="https://www.linkedin.com/in/viskirubakaran/">
-          <FontAwesomeIcon
-            icon={faLinkedin}
-            className="ease-in-out-anim pr-2 text-3xl text-white"
-          />
-        </a>
-      </div>
-    </footer>
+    <div className="m-2 rounded-xl">
+      <footer id="contact">
+        <p className="pb-2 text-lg uppercase text-white">Connect with me</p>
+        <div>
+          <a href="https://github.com/vikiru">
+            <FontAwesomeIcon
+              icon={faGithub}
+              className="ease-in-out-anim pr-2 text-3xl text-white"
+            />
+          </a>
+          <a href="https://www.linkedin.com/in/viskirubakaran/">
+            <FontAwesomeIcon
+              icon={faLinkedin}
+              className="ease-in-out-anim pr-2 text-3xl text-white"
+            />
+          </a>
+          <div className=" pt-1">
+            <p className="text-white">
+              <span>
+                Visakan Kirubakaran &copy; {currentYear}. All rights reserved.
+              </span>
+            </p>
+          </div>
+        </div>
+      </footer>
+    </div>
   );
 }
 
