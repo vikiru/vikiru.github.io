e91cd5d3ec37d4f63886bf0b01eaf6fabe731485
vikiru <72267229+vikiru@users.noreply.github.com>
2023-11-25 12:21:40 -0500

    Update ProjectContainer
    todo: Split Container into two separate components, ProjectCard and ProjectContainer

--- src/components/ProjectContainer/ProjectContainer.js
+++ src/components/ProjectContainer/ProjectContainer.js
@@ -1,74 +1,80 @@
+import "./ProjectContainer.css";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import { faBookOpen } from "@fortawesome/free-solid-svg-icons";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import projectData from "../../assets/projects";
+import shieldImages from "../../assets/shieldImages";
 
 console.log(projectData);
+console.log(shieldImages);
+console.log(shieldImages[`Java`]);
 
 function ProjectContainer() {
   const projects = projectData.projects;
   const projectDisplay = (
-    <div>
-      <div className="bg-black">
-        <h1 className="text-left text-xl text-white underline">Projects</h1>
-        <div className="flex flex-col">
-          {projects.map((project) => (
-            <div
-              className="m-2 rounded-xl bg-blue-400 p-5 text-white"
-              key={project}
-            >
-              <div className="flex justify-between">
-                <p className="text-md font-semibold uppercase text-slate-100">
-                  {project.name}
-                </p>
-                <p className="text-md text-right uppercase text-slate-100">
-                  {project.startDate} - {project.endDate}
-                </p>
-              </div>
-              <p className="text-left text-sm text-lime-200">
-                {project.description}
+    <div className="overflow-x- h-screen w-screen overflow-y-auto bg-black">
+      <h1 className="section-text">Projects</h1>
+      <div className="flex flex-col">
+        {/* Project Info Div */}
+        {projects.map((project) => (
+          <div className="project-card" key={project}>
+            {/* Project Name, Start - End Section */}
+            <div className="flex justify-between">
+              <p className="project-name">{project.name}</p>
+              <p className="project-dates">
+                {project.startDate} - {project.endDate}
               </p>
-              <div className="flex">
+            </div>
+            <p className="project-desc">{project.description}</p>
+            <div className="2xl:flex 2xl:justify-between">
+              {/* Technologies Used within Project Section */}
+              <div className="grid grid-cols-1 lg:flex">
+                {project.technologiesUsed.map((technology) =>
+                  shieldImages[`${technology}`] != "" ? (
+                    <img
+                      key={technology}
+                      src={shieldImages[`${technology}`]}
+                      className="shield-image"
+                    ></img>
+                  ) : null,
+                )}
+              </div>
+              {/* Project URLs */}
+              <div className="pt-4 2xl:flex">
                 <a href={project.githubUrl}>
                   <FontAwesomeIcon
                     icon={faGithub}
-                    className="pr-2 pt-4 text-3xl"
+                    className="social-icon pr-1"
                   />
                 </a>
                 {project.documentationUrl != "" && (
                   <a href={project.documentationUrl}>
                     <FontAwesomeIcon
                       icon={faBookOpen}
-                      className="pt-4 text-3xl"
+                      className="social-icon"
                     />
                   </a>
                 )}
               </div>
-              <details>
-                <summary className="text-black">
-                  View My Accomplishments
-                </summary>
-                <ul className="list-disc">
-                  {project.accomplishments.map((accomplishment) => (
-                    <li
-                      className="text-left text-sm text-black"
-                      key={accomplishment}
-                    >
-                      {accomplishment}
-                    </li>
-                  ))}
-                </ul>
-                <div className="flex">
-                  {project.technologiesUsed.map((technology) => (
-                    <p key={technology}>{technology}</p>
-                  ))}
-                </div>
-              </details>
             </div>
-          ))}
-          ;
-        </div>
+            <details>
+              <summary className=" text-black">View My Accomplishments</summary>
+              <ul className="list-disc">
+                {project.accomplishments.map((accomplishment) => (
+                  <li
+                    className="text-left text-sm text-black"
+                    key={accomplishment}
+                  >
+                    {accomplishment}
+                  </li>
+                ))}
+              </ul>
+            </details>
+          </div>
+        ))}
+        ;
       </div>
     </div>
   );
