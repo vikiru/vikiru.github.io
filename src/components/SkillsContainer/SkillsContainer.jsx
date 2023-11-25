f6b8c8a43ccd1587386a023e6bd15a37605a5573
vikiru <72267229+vikiru@users.noreply.github.com>
2023-11-25 18:07:44 -0500

    Update SkillsContainer.jsx

--- src/components/SkillsContainer/SkillsContainer.jsx
+++ src/components/SkillsContainer/SkillsContainer.jsx
@@ -6,16 +6,16 @@ function SkillsContainer() {
   return (
     <div className="bg-black-400" id="skills">
       <p className="section-text">Skills</p>
-      <div className="overflow-x-hidden overflow-y-hidden 2xl:grid 2xl:grid-cols-11">
+      <div className="m-2 overflow-x-hidden overflow-y-hidden rounded-xl bg-blue-400 2xl:grid 2xl:grid-cols-7">
         {Object.keys(skillsData).map((key, index) => (
-          <div className=" bg-blue-400 sm:flex 2xl:block" key={index}>
+          <div className="sm:flex 2xl:block" key={index}>
             <p className="rounded uppercase text-white">{key}</p>
             {skillsData[key].map((skill) =>
               shieldImages[`${skill}`] != "" ? (
                 <div key={skill}>
                   <img
                     src={shieldImages[`${skill}`]}
-                    className="shield-image"
+                    className="shield-image w-full"
                     alt={skill}
                   ></img>
                 </div>
