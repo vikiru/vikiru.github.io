b9872a7053447f8d5fc12a231782ab395f160a64
vikiru <72267229+vikiru@users.noreply.github.com>
2023-11-25 21:59:37 -0500

    Add extra small screen size and make nav responsive accordingly

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./src/**/*.{html,jsx}"],
   theme: {
-    extend: {},
+    screens: {
+      xxs: "300px",
+      ...defaultTheme.screens,
+    },
   },
+  extend: {},
   plugins: [],
 };
