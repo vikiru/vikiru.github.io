2f7cf8623035451cc745ac8ac3c5ef6052794b6b
vikiru <72267229+vikiru@users.noreply.github.com>
2023-11-25 22:32:27 -0500

    Add purge property to tailwind config to get rid of unused css

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   content: ["./src/**/*.{html,jsx}"],
+  purge: ["./src/**/*.{html,jsx}"],
   theme: {
     screens: {
       xs: "300px",
