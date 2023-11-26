bd6c02c1669b0811419c262a12e15f1d689aaabb
vikiru <72267229+vikiru@users.noreply.github.com>
2023-11-26 16:55:34 -0500

    Update SkillsContainer responsiveness

--- src/components/SkillsContainer/SkillsContainer.jsx
+++ src/components/SkillsContainer/SkillsContainer.jsx
@@ -6,19 +6,19 @@ function SkillsContainer() {
   return (
     <section className="snap-center" id="skills">
       <p className="section-text">Skills</p>
-      <section className="m-2 overflow-x-hidden overflow-y-hidden rounded-xl bg-sky-500/80 2xl:grid 2xl:grid-cols-7">
+      <section className="m-2 overflow-x-hidden overflow-y-hidden rounded-xl bg-sky-500/80 xs:grid xs:grid-cols-4 2xl:grid 2xl:grid-cols-7">
         {Object.keys(skillsData).map((key) => (
-          <section className="sm:flex 2xl:block" key={key}>
+          <section className="xs:m-2 2xl:block" key={key} id={key}>
             <p className="rounded uppercase text-white">{key}</p>
             {skillsData[key].map((skill) =>
               shieldImages[`${skill}`] != "" ? (
-                <div key={skill}>
+                <section key={skill}>
                   <img
                     src={shieldImages[`${skill}`]}
                     className="shield-image w-full"
                     alt={skill}
                   ></img>
-                </div>
+                </section>
               ) : null,
             )}
           </section>
