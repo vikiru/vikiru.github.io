c4bfd1b148001cf8073ffaac3552566614330413
vikiru <72267229+vikiru@users.noreply.github.com>
2023-11-26 16:09:28 -0500

    Update ProjectContainer.jsx

--- src/components/ProjectContainer/ProjectContainer.jsx
+++ src/components/ProjectContainer/ProjectContainer.jsx
@@ -12,7 +12,6 @@ function ProjectContainer() {
         {projects.map((project) => (
           <ProjectCard key={project.name} project={project} />
         ))}
-        ;
       </div>
     </section>
   );
