3a2c552c0921aa497674f1cc1f446fc9ac196485
vikiru <72267229+vikiru@users.noreply.github.com>
2023-11-27 13:35:29 -0500

    Add new font and styles to Education section

--- src/components/EducationContainer/EducationContainer.jsx
+++ src/components/EducationContainer/EducationContainer.jsx
@@ -8,13 +8,13 @@ function EducationContainer() {
   return (
     <section className="snap-center" id="education">
       <p className="section-text">Education</p>
-      <section className="m-2 overflow-x-hidden overflow-y-hidden rounded-xl bg-sky-500/80 p-2">
+      <section className="m-2 overflow-x-hidden overflow-y-hidden rounded-xl  p-2">
         <section
           id="university-enrollment-info"
           className="flex justify-between"
         >
           <section>
-            <p className="pl-1 font-sans text-lg uppercase text-black">
+            <p className="font-heading pl-1 text-lg uppercase text-gray-400">
               {educationData.universityName}
             </p>
           </section>
@@ -25,7 +25,7 @@ function EducationContainer() {
           </section>
         </section>
         <section id="university-degree" className="flex pb-1">
-          <p className="left-0 pl-1 font-mono italic text-slate-50">
+          <p className="font-subheading left-0 pl-1 italic text-slate-50">
             {educationData.degree} - {educationData.acronym}.,{" "}
             {educationData.degreeProgram}
           </p>
@@ -34,7 +34,7 @@ function EducationContainer() {
           <section id="certificates" key={certificate}>
             <div className="flex justify-between">
               <section>
-                <p className="font-sans text-lg uppercase text-black sm:pl-11 md:pl-1">
+                <p className="font-heading text-lg uppercase text-gray-400 sm:pl-11 md:pl-1">
                   {certificate.provider}
                 </p>
               </section>
@@ -55,7 +55,7 @@ function EducationContainer() {
                   >
                     <FontAwesomeIcon
                       icon={faLink}
-                      className="ease-in-out-anim text-md pr-2 text-black"
+                      className="ease-in-out-anim text-md pr-2 text-gray-400"
                     />
                   </a>
                 </span>
