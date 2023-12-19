732dd5946ecb5cc4a20d7adfd97cc96ca62f53d1
vikiru <72267229+vikiru@users.noreply.github.com>
2023-12-19 15:14:08 -0500

    Add initial ProjectShowcase page

--- src/pages/Router/Router.jsx
+++ src/pages/Router/Router.jsx
@@ -1,14 +1,25 @@
 import { Route, Routes } from "react-router-dom";
 
 import Home from "../Home/Home";
+import ProjectShowcase from "../ProjectShowcase/ProjectShowcase";
 import Projects from "../Projects/Projects";
 import React from "react";
+import projectData from "../../assets/data/projects";
 
 function Router() {
+  const projects = projectData.projects;
   return (
     <Routes>
       <Route path="/" element={<Home />}></Route>
       <Route path="/projects" element={<Projects />}></Route>
+      {projects.map((project) => (
+        <Route
+          key={project}
+          path={"/projects/" + project.name.toLowerCase()}
+          element={<ProjectShowcase project={project} />}
+        />
+      ))}
+      <Route path="/projects/Urvo" element={<ProjectShowcase />}></Route>
     </Routes>
   );
 }
