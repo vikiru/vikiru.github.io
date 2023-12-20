51357f541553d2fa2bb66e2b21ffa342196adfdf
vikiru <72267229+vikiru@users.noreply.github.com>
2023-12-19 19:21:05 -0500

    Add university section to courses page

--- src/pages/Courses/Courses.jsx
+++ src/pages/Courses/Courses.jsx
@@ -1,16 +1,34 @@
 import React from "react";
 import courseData from "./../../assets/data/courses";
+import educationData from "../../assets/data/education";
 
 function Courses() {
   return (
     <main className="h-screen w-screen snap-y overflow-x-hidden overflow-y-scroll scroll-smooth bg-gradient-to-r from-red-500 to-teal-400 pb-4 scrollbar scrollbar-track-teal-400 scrollbar-thumb-red-500">
-      <h2 className="pt-2 text-center text-2xl font-bold uppercase text-white underline">
-        Courses
-      </h2>
+      <div className="m-2 bg-white py-6">
+        <div className="mx-2 px-2">
+          <h2 className="text-left font-heading font-semibold uppercase">
+            {educationData.universityName}
+          </h2>
+        </div>
+        <div className="mx-2 flex justify-between">
+          <div className="max-w-lg px-2">
+            <h3 className="text-left font-subheading font-thin">
+              {educationData.degree} - {educationData.acronym}.,{" "}
+              {educationData.degreeProgram}
+            </h3>
+          </div>
+          <div className="max-w-xs">
+            <h3 className="pl-2 text-left font-body font-thin uppercase">
+              {educationData.enrollmentStart} - {educationData.enrollmentEnd}
+            </h3>
+          </div>
+        </div>
+      </div>
       <div>
         {courseData.years.map((year) => (
           <div key={year.title}>
-            <h3 className="pl-3 font-subheading text-xl font-semibold uppercase text-white underline xxs:text-center lg:text-left">
+            <h3 className="pl-3 font-subheading text-xl font-semibold uppercase text-black underline xxs:text-center lg:text-left">
               {year.title}
             </h3>
             <div className="md:grid md:grid-cols-2 lg:grid-cols-3 lg:overflow-x-auto">
