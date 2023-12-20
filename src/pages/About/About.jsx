c00fa1e72d8d48f851658e6a199473190cc17abd
vikiru <72267229+vikiru@users.noreply.github.com>
2023-12-19 19:21:18 -0500

    Update about spacing

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -92,25 +92,27 @@ function Home() {
             Education
           </h2>
         </div>
-        <div className="mx-2">
-          <h2 className="text-left font-heading font-semibold uppercase">
-            {educationData.universityName}
-          </h2>
-        </div>
-        <div className="mx-2 flex justify-between">
-          <div className="max-w-lg">
-            <h3 className="text-left font-subheading font-thin">
-              {educationData.degree} - {educationData.acronym}.,{" "}
-              {educationData.degreeProgram}
-            </h3>
+        <div className="m-2 bg-white py-2">
+          <div className="mx-2 px-2">
+            <h2 className="text-left font-heading font-semibold uppercase">
+              {educationData.universityName}
+            </h2>
           </div>
-          <div className="max-w-xs">
-            <h3 className="text-left font-body font-thin uppercase">
-              {educationData.enrollmentStart} - {educationData.enrollmentEnd}
-            </h3>
+          <div className="mx-2 flex justify-between">
+            <div className="max-w-lg px-2">
+              <h3 className="text-left font-subheading font-thin">
+                {educationData.degree} - {educationData.acronym}.,{" "}
+                {educationData.degreeProgram}
+              </h3>
+            </div>
+            <div className="max-w-xs">
+              <h3 className="pl-2 text-left font-body font-thin uppercase">
+                {educationData.enrollmentStart} - {educationData.enrollmentEnd}
+              </h3>
+            </div>
           </div>
         </div>
-        <div className="mx-2">
+        <div className="mx-2 flex xxs:justify-center lg:justify-start lg:pl-4">
           <button className="rounded bg-black px-4 py-2">
             <span className="text-md font-subheading uppercase text-white">
               View Courses
@@ -133,7 +135,7 @@ function Home() {
             Coursera
           </h2>
           {courseraCertificates.map((certificate) => (
-            <div key={certificate}>
+            <div key={certificate.certificationName}>
               <div className="flex justify-between">
                 <div className="max-w-lg">
                   <h3 className="font-subheading font-thin">
