beb689e5133395e4a1f1b151ec5b3c944369153c
Visakan Kirubakaran <72267229+vikiru@users.noreply.github.com>
2023-12-23 22:12:09 -0500

    Add Course page to navData

--- src/assets/data/nav.js
+++ src/assets/data/nav.js
@@ -7,6 +7,7 @@ const navData = [
     navTitle: "About",
     navLink: "/about",
   },
+  { navTitle: "Courses", navLink: "/education/courses" },
   {
     navTitle: "Projects",
     navLink: "/projects",
