e686db9ee1a943a0339d71804bc9b8e4b962d89d
Visakan Kirubakaran <72267229+vikiru@users.noreply.github.com>
2023-12-23 23:28:23 -0500

    Update home page layout

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,37 +4,52 @@ import React from "react";
 
 function HomePage() {
   return (
-    <div className="h-screen w-screen snap-y overflow-x-hidden overflow-y-scroll scroll-smooth scrollbar scrollbar-track-teal-400 scrollbar-thumb-red-500">
+    <div className="h-screen w-screen snap-y overflow-x-hidden overflow-y-scroll scroll-smooth bg-white">
       <NavBar />
-      <main>
-        <div className="mx-2 my-12 rounded-lg py-4 ">
-          <div className="max-w-5xl">
-            <h2 className="text-center font-heading text-8xl font-bold tracking-widest">
-              Visakan Kirubakaran
+      <main className="h-screen">
+        <div className="flex justify-center py-2">
+          <h2 className="font-cinzel text-8xl tracking-widest text-black">
+            Visakan Kirubakaran
+          </h2>
+        </div>
+
+        <div className="flex justify-center py-1">
+          <h3 className="font-heading text-5xl font-thin text-black">
+            Software Developer
+          </h3>
+        </div>
+
+        <div className="mx-auto max-w-lg py-1">
+          <p className="text-center font-body text-4xl text-black">
+            I have a passion for software design and web technologies. Committed
+            to learning and contributing to meaningful projects!
+          </p>
+        </div>
+
+        <div className="flex justify-center gap-2 py-4">
+          <div className="w-60 rounded-full bg-blue-400 p-2 hover:cursor-pointer hover:ring-4">
+            <a href="/about" target="_blank" rel="noopener noreferrer">
+              <h2 className="text-center text-2xl uppercase text-white">
+                Read More
+              </h2>
+            </a>
+          </div>
+          <div className="w-60 rounded-full bg-orange-400 p-2 hover:cursor-pointer hover:ring-4">
+            <h2 className="text-center text-2xl uppercase text-white">
+              View My Work
             </h2>
           </div>
-          <div>
-            <div className="m-4 mx-auto max-w-xs rounded-full bg-white hover:bg-black hover:text-white">
-              <a href="/" target="_blank" rel="noopener noreferrer">
-                <h2 className="text-center font-heading font-thin uppercase">
-                  Home
-                </h2>
-              </a>
-            </div>
-            <div className="duration-400 m-4 mx-auto max-w-xs rounded-full bg-white transition-colors hover:bg-black hover:text-white">
-              <a href="/about" target="_blank" rel="noopener noreferrer">
-                <h2 className="text-center font-heading font-thin uppercase">
-                  About
-                </h2>
-              </a>
-            </div>
-            <div className="m-4 mx-auto max-w-xs rounded-full bg-white hover:bg-black hover:text-white">
-              <a href="/projects" target="_blank" rel="noopener noreferrer">
-                <h2 className="text-center font-heading font-thin uppercase">
-                  Projects
-                </h2>
-              </a>
-            </div>
+          <div className="w-60 rounded-full bg-green-400 p-2 hover:cursor-pointer hover:ring-4">
+            <a
+              href="mailto:viskirubakaran@outlook.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              id="email"
+            >
+              <h2 className="text-center text-2xl uppercase text-white">
+                Contact Me
+              </h2>
+            </a>
           </div>
         </div>
       </main>
