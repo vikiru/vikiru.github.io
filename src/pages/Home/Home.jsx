987504f42e28a370a8898fd2feb31cf4e4a82264
Visakan Kirubakaran <72267229+vikiru@users.noreply.github.com>
2023-12-23 23:39:14 -0500

    Add location and adjust spacing in home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,25 +1,35 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Footer from "../../components/Footer/Footer";
 import NavBar from "./../../components/NavBar/NavBar";
 import React from "react";
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
 
 function HomePage() {
   return (
     <div className="h-screen w-screen snap-y overflow-x-hidden overflow-y-scroll scroll-smooth bg-white">
       <NavBar />
       <main className="h-screen">
-        <div className="flex justify-center py-2">
+        <div className="flex justify-center">
           <h2 className="font-cinzel text-8xl tracking-widest text-black">
             Visakan Kirubakaran
           </h2>
         </div>
 
-        <div className="flex justify-center py-1">
-          <h3 className="font-heading text-5xl font-thin text-black">
+        <div className="flex justify-center">
+          <h3 className="font-subheading text-5xl font-thin text-black">
             Software Developer
           </h3>
         </div>
 
-        <div className="mx-auto max-w-lg py-1">
+        <div className="flex justify-center py-1">
+          <FontAwesomeIcon
+            icon={faLocationDot}
+            className="px-2 pt-1 text-2xl"
+          />
+          <span className="font-subheading text-2xl">Ottawa, ON</span>
+        </div>
+
+        <div className="mx-auto max-w-xl py-1">
           <p className="text-center font-body text-4xl text-black">
             I have a passion for software design and web technologies. Committed
             to learning and contributing to meaningful projects!
