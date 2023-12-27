bc2c9468994e59a216550365d888e6448777b47f
vikiru <72267229+vikiru@users.noreply.github.com>
2023-12-27 16:27:31 -0500

    Apply colour palette changes to Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,20 +8,20 @@ import navData from "./../../assets/data/nav";
 function Footer() {
   return (
     <footer className="mt-2 w-full">
-      <div className="grid grid-cols-3 bg-black">
+      <div className="bg-secondary grid grid-cols-3">
         <div className="m-auto max-w-sm pr-4">
-          <h2 className="font-cinzel tracking-widest text-white xxs:text-lg md:text-5xl">
+          <h2 className="text-primary font-cinzel tracking-widest xxs:text-lg md:text-5xl">
             Visakan
           </h2>
         </div>
         <div>
-          <h2 className="text-center font-heading text-xl uppercase text-white lg:text-2xl">
+          <h2 className="text-primary text-center font-heading text-xl uppercase lg:text-2xl">
             Read More
           </h2>
           {navData.map((navItem) => (
             <div key={navItem} className="mx-auto w-20">
               <a href={navItem.navLink}>
-                <h2 className="text-center text-lg text-white hover:underline lg:text-xl">
+                <h2 className="text-primary text-center text-lg hover:underline lg:text-xl">
                   {navItem.navTitle}
                 </h2>
               </a>
@@ -29,11 +29,11 @@ function Footer() {
           ))}
         </div>
         <div className="mx-auto">
-          <h2 className="text-center font-heading text-xl uppercase text-white lg:text-2xl">
+          <h2 className="text-primary text-center font-heading text-xl uppercase lg:text-2xl">
             Contact
           </h2>
           <div>
-            <h2 className="py-2 text-center font-subheading text-lg text-white lg:text-xl">
+            <h2 className="text-primary py-2 text-center font-subheading text-lg lg:text-xl">
               Ottawa, ON
             </h2>
             <a
@@ -44,7 +44,7 @@ function Footer() {
             >
               <FontAwesomeIcon
                 icon={faGithub}
-                className="lg:ease-in-out-anim px-2 py-4 text-3xl text-white lg:text-5xl"
+                className="lg:ease-in-out-anim text-primary px-2 py-4 text-3xl lg:text-5xl"
               />
             </a>
             <a
@@ -55,7 +55,7 @@ function Footer() {
             >
               <FontAwesomeIcon
                 icon={faLinkedin}
-                className="lg:ease-in-out-anim px-2 py-4 text-3xl text-white lg:text-5xl"
+                className="lg:ease-in-out-anim text-primary px-2 py-4 text-3xl lg:text-5xl"
               />
             </a>
             <a
@@ -66,16 +66,15 @@ function Footer() {
             >
               <FontAwesomeIcon
                 icon={faEnvelope}
-                className="lg:ease-in-out-anim px-2 py-4 text-3xl text-white lg:text-5xl"
+                className="lg:ease-in-out-anim text-primary px-2 py-4 text-3xl lg:text-5xl"
               />
             </a>
           </div>
         </div>
       </div>
-      <div className="bg-black">
-        <hr className="bg-white"></hr>
-        <div className="mx-auto flex max-w-sm items-center py-2">
-          <span className="text-center font-heading font-thin text-white">
+      <div className="bg-secondary">
+        <div className="mx-auto flex max-w-lg items-center justify-center py-2">
+          <span className="text-primary text-center font-heading font-thin lg:text-lg">
             Visakan Kirubakaran &copy; {new Date().getFullYear()}. All rights
             reserved.
           </span>
