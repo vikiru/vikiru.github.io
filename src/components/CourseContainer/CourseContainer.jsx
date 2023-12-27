7a3b19b682e292bc730e6bb84de8bb95a99d080f
vikiru <72267229+vikiru@users.noreply.github.com>
2023-12-27 17:31:53 -0500

    Replace University info section

--- src/components/CourseContainer/CourseContainer.jsx
+++ src/components/CourseContainer/CourseContainer.jsx
@@ -6,33 +6,31 @@ import educationData from "../../assets/data/education";
 function CourseContainer() {
   return (
     <section id="university-courses">
-      <div className="bg-primary m-2 py-6">
+      <section className="m-2 mx-auto max-w-lg" id="unversity-info">
         <div className="mx-2 px-2">
-          <h2 className="text-secondary text-left font-heading font-semibold uppercase lg:text-3xl">
+          <h2 className="text-center font-heading font-semibold uppercase lg:text-4xl">
             {educationData.universityName}
           </h2>
         </div>
-        <div className="mx-2 flex justify-between">
-          <div className="px-2 max-w-md">
-            <h3 className="text-secondary text-left font-subheading font-thin lg:text-2xl">
-              {educationData.degree} - {educationData.acronym}.,{" "}
-              {educationData.degreeProgram}
-            </h3>
-          </div>
-          <div className="max-w-md">
-            <h3 className="px-2 text-left font-body uppercase lg:text-xl">
-              {educationData.enrollmentStart} - {educationData.enrollmentEnd}
-            </h3>
-          </div>
+        <div className="mx-auto max-w-sm">
+          <h3 className="pl-2 text-center font-body uppercase lg:text-2xl">
+            {educationData.enrollmentStart} - {educationData.enrollmentEnd}
+          </h3>
         </div>
-      </div>
+        <div className="max-w-lg px-2">
+          <h3 className="text-center font-subheading lg:text-3xl">
+            {educationData.degree} - {educationData.acronym}.,{" "}
+            {educationData.degreeProgram}
+          </h3>
+        </div>
+      </section>
       <div>
         {courseData.years.map((year) => (
           <div key={year.title} className="py-2 ">
             <h3 className="text-primary bg-secondary pl-3 font-subheading text-xl font-semibold uppercase underline xxs:text-center lg:text-4xl">
               {year.title}
             </h3>
-            <div className="grid gap-3 pt-2 px-2 lg:grid-cols-2">
+            <div className="grid gap-3 px-2 pt-2 lg:grid-cols-2">
               {year.courses.map((course) => (
                 <CourseCard key={course} course={course} />
               ))}
