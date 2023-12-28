5bb3eea0e51bcced2e73e826302cf02c5fd98fec
vikiru <72267229+vikiru@users.noreply.github.com>
2023-12-28 13:09:03 -0500

    Update logo on Footer and Nav

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -14,11 +14,20 @@ function NavBar() {
 
   return (
     <nav>
-      <section id="navigation-menu" className="bg-secondary py-4">
+      <section
+        id="navigation-menu"
+        className="flex justify-between bg-secondary py-4"
+      >
+        <div className="mx-auto">
+          <h2 className="font-cinzel tracking-widest text-primary xxs:text-lg md:text-3xl">
+            <span className="text-primary">Visa</span>
+            <span className="text-accent">kan</span>
+          </h2>
+        </div>
         <div className="flex justify-center">
           {navData.map((nav) => (
             <a key={nav} href={nav.navLink}>
-              <h2 className="text-primary px-2 text-lg uppercase hover:underline lg:text-2xl">
+              <h2 className="px-2 text-lg uppercase tracking-widest text-primary hover:underline hover:decoration-accent lg:text-2xl">
                 {nav.navTitle}
               </h2>
             </a>
