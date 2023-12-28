8c624d9701653e176d13c5cc6cd60c23cc6ad55f
vikiru <72267229+vikiru@users.noreply.github.com>
2023-12-28 12:59:13 -0500

    Re-arrange Coursera certificate section to 2 col

--- src/components/EducationContainer/EducationContainer.jsx
+++ src/components/EducationContainer/EducationContainer.jsx
@@ -24,15 +24,15 @@ function EducationContainer() {
             {educationData.universityName}
           </h2>
         </div>
-        <div className="mx-auto max-w-sm pt-1">
-          <h3 className="pl-2 text-center font-body uppercase lg:text-xl">
-            {educationData.enrollmentStart} - {educationData.enrollmentEnd}
-          </h3>
-        </div>
-        <div className="max-w-lg px-2 pb-2">
+        <div className="max-w-lg px-2">
           <h3 className="text-center font-subheading lg:text-2xl">
             {educationData.degree} - {educationData.acronym}.,{" "}
             {educationData.degreeProgram}
+          </h3>
+        </div>
+        <div className="mx-auto max-w-sm pt-1">
+          <h3 className="pl-2 text-center font-body font-thin uppercase lg:text-xl">
+            {educationData.enrollmentStart} - {educationData.enrollmentEnd}
           </h3>
         </div>
 
@@ -56,47 +56,49 @@ function EducationContainer() {
       </section>
 
       {/* Coursera Section - XXS - XL Screens */}
-      <section className="m-2 mx-auto max-w-lg" id="coursera-certificate-info">
+      <section className="m-2 mx-auto" id="coursera-certificate-info">
         <div className="mx-2 px-2">
           <h2 className="text-center font-heading font-semibold uppercase lg:text-3xl">
             Coursera
           </h2>
         </div>
-        {courseraCertificates.map((certificate) => (
-          <div key={certificate.certificationName} className="pb-4 pt-1">
-            <div>
-              <div className="mx-2 max-w-lg">
-                <div className="mx-auto max-w-xs">
-                  <p className="text-md pl-2 text-center font-body uppercase lg:text-xl">
-                    {certificate.startDate} - {certificate.completionDate}
-                  </p>
-                </div>
-                <div>
-                  <h3 className="text-center font-subheading lg:text-2xl">
-                    {certificate.certificationName}
-                  </h3>
+        <div className="lg:grid lg:grid-cols-2">
+          {courseraCertificates.map((certificate) => (
+            <div key={certificate.certificationName} className="pb-4 pt-1">
+              <div>
+                <div className="mx-2 max-w-lg">
+                  <div>
+                    <h3 className="text-center font-subheading lg:text-2xl">
+                      {certificate.certificationName}
+                    </h3>
+                  </div>
+                  <div className="mx-auto max-w-xs">
+                    <p className="text-md pl-2 text-center font-body font-thin uppercase lg:text-xl">
+                      {certificate.startDate} - {certificate.completionDate}
+                    </p>
+                  </div>
                 </div>
               </div>
+              <div className="mx-2 flex bg-accent py-1 xxs:justify-center">
+                <button className="bg-black rounded px-4 py-2">
+                  <span className="text-md text-white font-subheading uppercase tracking-widest">
+                    View Certificate
+                  </span>
+                  <a
+                    href={certificate.certificateLink}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <FontAwesomeIcon
+                      className="lg:ease-in-out-anim text-white pl-1 text-lg"
+                      icon={faArrowUpRightFromSquare}
+                    />
+                  </a>
+                </button>
+              </div>
             </div>
-            <div className="mx-2 flex bg-accent py-1 xxs:justify-center">
-              <button className="bg-black rounded px-4 py-2">
-                <span className="text-md text-white font-subheading uppercase tracking-widest">
-                  View Certificate
-                </span>
-                <a
-                  href={certificate.certificateLink}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <FontAwesomeIcon
-                    className="lg:ease-in-out-anim text-white pl-1 text-lg"
-                    icon={faArrowUpRightFromSquare}
-                  />
-                </a>
-              </button>
-            </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </section>
     </section>
   );
