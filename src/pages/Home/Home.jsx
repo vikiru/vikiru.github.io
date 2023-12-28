907560612dee45d3e014a9b0e082a911e112f5ca
vikiru <72267229+vikiru@users.noreply.github.com>
2023-12-28 13:16:15 -0500

    Add social icons and profile picture to home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,22 +1,30 @@
+import { faEnvelope, faLocationDot } from "@fortawesome/free-solid-svg-icons";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Footer from "../../components/Footer/Footer";
 import NavBar from "./../../components/NavBar/NavBar";
 import React from "react";
-import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
 
 function HomePage() {
   return (
-    <div className="bg-primary scrollbar-track-primary scrollbar-thumb-secondary h-screen w-screen snap-y overflow-x-hidden overflow-y-scroll scroll-smooth scrollbar">
+    <div className="h-screen w-screen snap-y overflow-x-hidden overflow-y-scroll scroll-smooth bg-primary scrollbar scrollbar-track-primary scrollbar-thumb-secondary">
       <NavBar />
-      <main>
+      <main className="py-4">
         <div className="flex justify-center">
-          <h2 className="text-secondary text-center font-cinzel tracking-widest lg:text-8xl">
+          <h2 className="text-center font-cinzel tracking-widest text-secondary lg:text-6xl">
             Visakan Kirubakaran
           </h2>
         </div>
+        <div className="mx-auto my-1 flex max-w-sm justify-center rounded-full">
+          <img
+            className="object-fit h-40 w-40 rounded-full ring-4"
+            src={"https://avatars.githubusercontent.com/u/72267229?v=4"}
+          ></img>
+        </div>
 
         <div className="mx-auto flex max-w-lg justify-center">
-          <h3 className="text-secondary font-subheading text-5xl font-thin">
+          <h3 className="font-subheading font-thin text-secondary lg:text-4xl">
             Software Developer
           </h3>
         </div>
@@ -24,43 +32,78 @@ function HomePage() {
         <div className="flex justify-center py-1">
           <FontAwesomeIcon
             icon={faLocationDot}
-            className="px-2 pt-1 text-2xl"
+            className="px-2 pt-1 text-2xl text-accent"
           />
-          <span className="text-secondary font-subheading text-2xl">
+          <span className="font-subheading text-2xl tracking-wide text-secondary">
             Ottawa, ON
           </span>
         </div>
 
+        <div id="social-icons" className="flex justify-center space-x-4 pt-2">
+          <a
+            href="https://github.com/vikiru"
+            target="_blank"
+            rel="noopener noreferrer"
+            id="github-url"
+          >
+            <FontAwesomeIcon
+              icon={faGithub}
+              className="lg:ease-in-out-anim text-white text-3xl text-accent lg:text-5xl"
+            />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/viskirubakaran/"
+            target="_blank"
+            rel="noopener noreferrer"
+            id="linkedin-url"
+          >
+            <FontAwesomeIcon
+              icon={faLinkedin}
+              className="lg:ease-in-out-anim text-white text-3xl text-accent lg:text-5xl"
+            />
+          </a>
+          <a
+            href="mailto:viskirubakaran@outlook.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            id="email"
+          >
+            <FontAwesomeIcon
+              icon={faEnvelope}
+              className="lg:ease-in-out-anim text-white text-3xl text-accent lg:text-5xl"
+            />
+          </a>
+        </div>
         <div className="mx-auto max-w-xl py-1">
-          <p className="text-secondary text-center font-body text-4xl">
+          <p className="text-center font-body text-3xl text-secondary">
             I have a passion for software design and web technologies. Committed
             to learning and contributing to meaningful projects!
           </p>
         </div>
 
         <div className="flex justify-center gap-2 py-4">
-          <div className="bg-accent w-60 rounded-full bg-blue-400 p-2 hover:cursor-pointer hover:ring-4">
+          <div className="bg-blue-400 w-60 rounded-full bg-accent p-2 hover:cursor-pointer hover:ring-4">
             <a href="/about" target="_blank" rel="noopener noreferrer">
-              <h2 className="text-secondary text-center text-2xl uppercase">
+              <h2 className="text-center text-2xl uppercase text-secondary">
                 Read More
               </h2>
             </a>
           </div>
-          <div className="bg-accent w-60 rounded-full p-2 hover:cursor-pointer hover:ring-4">
+          <div className="w-60 rounded-full bg-accent p-2 hover:cursor-pointer hover:ring-4">
             <a href="/projects" target="_blank" rel="noopener noreferrer">
-              <h2 className="text-secondary text-center text-2xl uppercase tracking-widest">
+              <h2 className="text-center text-2xl uppercase tracking-widest text-secondary">
                 View My Work
               </h2>
             </a>
           </div>
-          <div className="bg-accent w-60 rounded-full p-2 hover:cursor-pointer hover:ring-4">
+          <div className="w-60 rounded-full bg-accent p-2 hover:cursor-pointer hover:ring-4">
             <a
               href="mailto:viskirubakaran@outlook.com"
               target="_blank"
               rel="noopener noreferrer"
               id="email"
             >
-              <h2 className="text-secondary text-center text-2xl uppercase tracking-widest">
+              <h2 className="text-center text-2xl uppercase tracking-widest text-secondary">
                 Contact Me
               </h2>
             </a>
