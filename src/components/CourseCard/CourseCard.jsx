b68d377364597dbb8da9a989c772c097ae0ed24a
vikiru <72267229+vikiru@users.noreply.github.com>
2023-12-30 13:05:46 -0500

    Make course page responsive on smaller and larger screens

--- src/components/CourseCard/CourseCard.jsx
+++ src/components/CourseCard/CourseCard.jsx
@@ -6,21 +6,21 @@ function CourseCard(props) {
   return (
     <div
       key={course.courseCode}
-      className="bg-white mx-auto max-w-lg rounded-md bg-primary px-4 pt-4 shadow-md ring-2 lg:pb-4"
+      className="bg-white mx-auto rounded-md bg-primary shadow-md ring-2 md:px-4 md:pt-4 lg:pb-4"
     >
       <div>
-        <h3 className="text-center font-semibold lg:text-3xl">
+        <h3 className="text-center font-semibold xxs:text-sm lg:text-3xl">
           {course.courseCode}
         </h3>
         {/* Course Name */}
-        <h4 className="py-2 text-center font-subheading text-sm lg:text-2xl">
+        <h4 className="py-2 text-center font-subheading xxs:text-sm lg:text-2xl">
           {course.name}
         </h4>
       </div>
 
       {/* Course Description */}
       <div className="mx-auto pb-4">
-        <p className="text-center font-body text-sm leading-6 lg:text-xl">
+        <p className="text-center font-body leading-6 xxs:text-xs sm:text-sm lg:text-xl">
           {course.description}
         </p>
       </div>
