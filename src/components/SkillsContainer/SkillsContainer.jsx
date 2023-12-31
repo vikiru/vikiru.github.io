6b6e66c18c63b21d8b7d87e80342afdc1b8a58a7
vikiru <72267229+vikiru@users.noreply.github.com>
2023-12-31 15:58:51 -0500

    Cleanup skills section styling and html elements

--- src/components/SkillsContainer/SkillsContainer.jsx
+++ src/components/SkillsContainer/SkillsContainer.jsx
@@ -3,18 +3,18 @@ import skillsData from "../../assets/data/skills";
 
 function SkillsContainer() {
   return (
-    <section className="mx-auto py-4" id="skills">
-      <div className="mx-auto">
-        <h2 className="pt-2 text-center text-2xl font-bold uppercase underline lg:text-4xl dark:text-darkSecondary">
+    <section className="mx-auto min-h-screen" id="skills">
+      <div className="px-4">
+        <h2 className="text-2xl font-bold uppercase underline lg:text-4xl dark:text-darkSecondary">
           Skills
         </h2>
-        <div className="mx-auto lg:grid lg:grid-cols-2">
+        <div className="lg:grid lg:grid-cols-2">
           {Object.keys(skillsData).map((skillCategory) => (
-            <div key={skillCategory} className="mx-auto max-w-sm pb-2">
-              <h2 className="text-center font-heading text-xl font-semibold uppercase lg:text-3xl dark:text-darkSecondary">
+            <div key={skillCategory} className="py-2" id={skillCategory}>
+              <h2 className="font-heading text-xl font-semibold uppercase lg:text-3xl dark:text-darkSecondary">
                 {skillCategory}
               </h2>
-              <p className="text-center font-subheading text-lg font-thin leading-6 text-secondary lg:text-2xl dark:text-darkSecondary">
+              <p className=" font-subheading text-lg leading-6 text-secondary lg:text-2xl dark:text-darkSecondary">
                 {skillsData[`${skillCategory}`].join(", ")}
               </p>
             </div>
