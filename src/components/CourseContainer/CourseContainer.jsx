4c95b39c0116308f6d411057fe9f09224aaf675e
vikiru <72267229+vikiru@users.noreply.github.com>
2024-01-03 17:46:06 -0500

    Remove vertical padding on year title in courses page

--- src/components/CourseContainer/CourseContainer.jsx
+++ src/components/CourseContainer/CourseContainer.jsx
@@ -7,7 +7,7 @@ function CourseContainer() {
   return (
     <section id="university-courses">
       <section id="unversity-info" className="min-w-screen px-4 py-2">
-        <div>
+        <div className="pt-4">
           <h2 className="heading-text uppercase dark:text-primary">
             {educationData.universityName}
           </h2>
@@ -31,7 +31,7 @@ function CourseContainer() {
             id={year.title.toLowerCase() + " courses"}
             className="py-6"
           >
-            <h3 className="heading-text bg-secondary px-4 py-6 uppercase text-primary underline dark:bg-darkSecondary dark:text-primary">
+            <h3 className="heading-text bg-secondary px-4 uppercase text-primary underline lg:py-6 dark:bg-darkSecondary dark:text-primary">
               {year.title}
             </h3>
             <div className="grid gap-3 px-2 pt-2 lg:grid-cols-2">
