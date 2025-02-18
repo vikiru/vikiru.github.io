368acd2a8b747bacf032127b1fcdb2d6ceb633b4
vikiru <72267229+vikiru@users.noreply.github.com>
2025-02-17 21:38:34 -0500

    Cleanup font sizes with clamp on home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import Footer from '~components/Footer/Footer';
 import NavBar from '~components/NavBar/NavBar';
 
@@ -7,10 +8,16 @@ function HomePage() {
             <NavBar />
             <div className="flex min-h-screen grow items-center justify-center">
                 <div className="m-2 xs:max-w-sm sm:max-w-md md:max-w-2xl lg:max-w-4xl xl:max-w-5xl 2xl:max-w-7xl 5xl:max-w-[88rem] 7xl:max-w-[104rem] 9xl:max-w-[160rem]">
-                    <h1 className="font-heading font-bold xs:text-xl xs:tracking-wide sm:text-2xl sm:tracking-tight lg:text-5xl 2xl:text-6xl 7xl:text-8xl 9xl:text-9xl">
+                    <h1
+                        className="font-heading font-bold xs:tracking-wide sm:tracking-tight"
+                        style={{ fontSize: 'clamp(1.25rem, 2.5vw, 9rem)' }}
+                    >
                         Hello, my name is Visakan Kirubakaran
                     </h1>
-                    <p className="font-body leading-relaxed xs:text-lg sm:text-xl lg:text-2xl 2xl:text-4xl 7xl:text-5xl 9xl:text-7xl">
+                    <p
+                        className="font-body leading-relaxed"
+                        style={{ fontSize: 'clamp(1rem, 1.5vw, 4.5rem)' }}
+                    >
                         I am a dedicated software developer with a passion for
                         software and web development. Utilizing my knowledge and
                         skills, I wish to challenge myself as I contribute to
@@ -30,24 +37,33 @@ function HomePage() {
                     </p>
 
                     <div className="flex gap-4 pt-4 xs:flex-col md:flex-row lg:justify-start">
-                        <a
-                            className="rounded-lg border-b-4 border-blue-500 bg-black px-6 py-3 text-center font-body font-medium text-white shadow-sm hover:bg-gray-800 xs:text-base lg:text-xl 2xl:text-2xl 7xl:border-b-[10px] 7xl:text-4xl 8xl:border-b-[15px] 8xl:px-12 8xl:py-6 9xl:px-12 9xl:py-12 9xl:text-6xl"
-                            href="/about"
+                        <Link
+                            className="rounded-lg border-b-4 border-blue-500 bg-black px-6 py-3 text-center font-body font-medium text-white shadow-sm hover:bg-gray-800 7xl:border-b-[10px] 8xl:border-b-[15px] 8xl:px-12 8xl:py-6 9xl:px-12"
+                            style={{
+                                fontSize: 'clamp(1rem, 1.125vw, 3.75rem)',
+                            }}
+                            to="/about"
                         >
                             Read more about me
-                        </a>
-                        <a
-                            className="rounded-lg border-b-4 border-blue-500 bg-black px-6 py-3 text-center font-body font-medium text-white shadow-sm hover:bg-gray-800 xs:text-base lg:text-xl 2xl:text-2xl 7xl:border-b-[10px] 7xl:text-4xl 8xl:border-b-[15px] 8xl:px-12 8xl:py-6 9xl:px-12 9xl:py-12 9xl:text-6xl"
-                            href="/projects"
+                        </Link>
+                        <Link
+                            className="rounded-lg border-b-4 border-blue-500 bg-black px-6 py-3 text-center font-body font-medium text-white shadow-sm hover:bg-gray-800 7xl:border-b-[10px] 8xl:border-b-[15px] 8xl:px-12 8xl:py-6 9xl:px-12"
+                            style={{
+                                fontSize: 'clamp(1rem, 1.125vw, 3.75rem)',
+                            }}
+                            to="/projects"
                         >
                             View my work
-                        </a>
-                        <a
-                            className="rounded-lg border-b-4 border-blue-500 bg-black px-6 py-3 text-center font-body font-medium text-white shadow-sm hover:bg-gray-800 xs:text-base lg:text-xl 2xl:text-2xl 7xl:border-b-[10px] 7xl:text-4xl 8xl:border-b-[15px] 8xl:px-12 8xl:py-6 9xl:px-12 9xl:py-12 9xl:text-6xl"
-                            href="/contact"
+                        </Link>
+                        <Link
+                            className="rounded-lg border-b-4 border-blue-500 bg-black px-6 py-3 text-center font-body font-medium text-white shadow-sm hover:bg-gray-800 7xl:border-b-[10px] 8xl:border-b-[15px] 8xl:px-12 8xl:py-6 9xl:px-12"
+                            style={{
+                                fontSize: 'clamp(1rem, 1.125vw, 3.75rem)',
+                            }}
+                            to="/contact"
                         >
                             Contact me
-                        </a>
+                        </Link>
                     </div>
                 </div>
             </div>
