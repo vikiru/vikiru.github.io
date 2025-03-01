de0758b9624aee3f548ec8d21665f4036004320c
vikiru <72267229+vikiru@users.noreply.github.com>
2025-02-28 19:32:04 -0500

    Update courses section, redesigned course cards, some responsiveness. Pending >3000 px responsiveness

--- src/components/CourseContainer/CourseContainer.tsx
+++ src/components/CourseContainer/CourseContainer.tsx
@@ -4,49 +4,96 @@ import CourseCard from '~components/CourseCard/CourseCard';
 
 function CourseContainer() {
     return (
-        <section id="university-courses">
-            <section className="min-w-screen px-4 py-2" id="unversity-info">
-                <div className="pt-4">
-                    <h2 className="heading-text uppercase dark:text-primary">
-                        {educationData.universityName}
+        <section className="bg-gray-50">
+            <section>
+                <div className="pb-2">
+                    <h2
+                        className="text-center font-heading font-semibold text-gray-800"
+                        style={{ fontSize: 'clamp(2rem, 2vw, 9rem)' }}
+                    >
+                        Education
                     </h2>
                 </div>
 
-                <div>
-                    <h3 className="subheading-text dark:text-primary">
-                        {educationData.degree} - {educationData.acronym}.,{' '}
-                        {educationData.degreeProgram}
-                    </h3>
-                </div>
-
-                <div>
-                    <h3 className="body-text uppercase dark:text-primary">
-                        {educationData.enrollmentStart} -{' '}
-                        {educationData.enrollmentEnd}
-                    </h3>
-                </div>
-            </section>
-
-            <section className="pb-6" id="course-container">
-                {courseData.years.map((year) => (
-                    <section
-                        className="py-6"
-                        id={year.title.toLowerCase() + ' courses'}
-                        key={year.title}
-                    >
-                        <h3 className="heading-text bg-secondary px-4 text-center uppercase text-primary underline lg:py-6 dark:bg-darkSecondary dark:text-primary">
-                            {year.title}
-                        </h3>
-                        <div className="grid gap-3 px-2 pt-2 lg:grid-cols-2">
-                            {year.courses.map((course) => (
-                                <CourseCard
-                                    course={course}
-                                    key={course.courseCode}
-                                />
-                            ))}
+                <div className="mx-2">
+                    <div className="mx-2 flex items-center justify-between">
+                        <div>
+                            <h2
+                                className="font-heading font-semibold"
+                                style={{ fontSize: 'clamp(1rem, 1.5vw, 4rem)' }}
+                            >
+                                {educationData.universityName}
+                            </h2>
                         </div>
-                    </section>
-                ))}
+                        <div>
+                            <p
+                                className="font-body text-gray-700"
+                                style={{
+                                    fontSize: 'clamp(0.65rem, 1.25vw, 4rem)',
+                                }}
+                            >
+                                {educationData.location}
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className="mx-2 flex items-center justify-between">
+                        <div>
+                            <h3
+                                className="font-subheading italic"
+                                style={{
+                                    fontSize: 'clamp(0.65rem, 1.25vw, 4rem)',
+                                }}
+                            >
+                                {educationData.degree} - {educationData.acronym}
+                                , {educationData.degreeProgram}
+                            </h3>
+                        </div>
+
+                        <div>
+                            <p
+                                className="font-body italic text-gray-500"
+                                style={{
+                                    fontSize: 'clamp(0.65rem, 1.25vw, 4rem)',
+                                }}
+                            >
+                                {educationData.enrollmentStart} -{' '}
+                                {educationData.enrollmentEnd}
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className="mt-3">
+                        {courseData.years.map((year) => (
+                            <div key={year.title}>
+                                <div className="flex items-center justify-center pb-4">
+                                    <h3
+                                        className="font-heading font-semibold"
+                                        style={{
+                                            fontSize:
+                                                'clamp(1rem, 1.75vw, 4rem)',
+                                        }}
+                                    >
+                                        {year.title}
+                                    </h3>
+                                </div>
+
+                                <div className="mx-2 grid gap-2 md:grid-cols-2">
+                                    {year.courses.map((course) => (
+                                        <CourseCard
+                                            course={course}
+                                            key={course.name}
+                                        />
+                                    ))}
+                                </div>
+
+                                <div
+                                    className={`mb-4 mt-8 ${year.title !== 'Fourth Year' ? 'border-t border-gray-300' : ''}`}
+                                />
+                            </div>
+                        ))}
+                    </div>
+                </div>
             </section>
         </section>
     );
