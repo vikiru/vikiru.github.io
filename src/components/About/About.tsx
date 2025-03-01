78eed24494792ec24cdcbed47879e592335a7729
Visakan Kirubakaran <72267229+vikiru@users.noreply.github.com>
2025-03-01 01:06:19 -0500

    Re-design about section, pending responsive >3000px changes (#266)

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,32 +1,40 @@
 function About() {
     return (
-        <section className="mx-auto pb-6" id="about">
-            <div className="pt-4">
-                <div className="px-4">
-                    <h2 className="heading-text pt-2 text-center uppercase underline dark:text-primary">
-                        About
-                    </h2>
-                </div>
+        <section className="min-h-[30vh] bg-gray-50">
+            <div className="pt-12">
+                <h2
+                    className="text-center font-heading font-semibold text-gray-800"
+                    style={{ fontSize: 'clamp(2rem, 2vw, 9rem)' }}
+                >
+                    About
+                </h2>
 
-                <div className="mx-auto px-4 pt-2">
-                    <p className="body-text my-2 text-secondary dark:text-primary">
-                        As a recent graduate in software engineering, I stand on
-                        the precipice of an exciting journey into the
-                        professional realm.
-                    </p>
-
-                    <p className="body-text my-2 text-secondary dark:text-primary">
-                        Equipped with a solid foundation of knowledge and a
-                        thirst for learning, I am eager to navigate the dynamic
-                        landscape of the industry. My readiness to embrace
-                        challenges, both familiar and unfamiliar, is fueled by a
-                        passion for problem-solving and innovation.
-                    </p>
-
-                    <p className="body-text mt-2 text-secondary dark:text-primary">
-                        I am committed to leveraging my skills and education to
-                        contribute to meaningful projects, fostering growth and
-                        driving progress.
+                <div className="max-w-screen-[12rem] m-4">
+                    <p
+                        className="font-body text-2xl leading-relaxed text-gray-600"
+                        style={{ fontSize: 'clamp(0.8rem, 1.25vw, 8rem)' }}
+                    >
+                        My journey into the world of software development began
+                        with an introduction to computer science and
+                        programming, utilizing languages such as C++ and Java.
+                        <br /> Following that, my university degree at Carleton
+                        University provided me with a strong foundation of
+                        knowledge and exposure to various languages, including
+                        Java, Python, HTML, CSS, JavaScript, C, Racket, and SQL.
+                        <br /> Since graduating, my goal has been to continue
+                        learning and growing as a software developer. I actively
+                        seek out new and meaningful projects to challenge
+                        myself, working with both familiar and unfamiliar
+                        technologies. My journey into web development has been
+                        particularly rewarding, where I’ve enjoyed working with
+                        technologies such as React, TailwindCSS, and Express.
+                        <br /> I love creating software, no matter the platform.
+                        The process of refining a rough idea into something
+                        polished and functional through iterative development
+                        excites me. I am particularly drawn to projects where
+                        the end product is easily accessible to anyone,
+                        regardless of their device, ensuring a wide-reaching
+                        impact.
                     </p>
                 </div>
             </div>
