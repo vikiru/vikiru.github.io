2df7a31eac42a14cbf84d798b69800cb0932bd60
vikiru <72267229+vikiru@users.noreply.github.com>
2025-03-11 18:27:09 -0400

    Fix responsiveness issues at larger screen size, >3000 px for contact page

--- src/components/ContactSection/index.tsx
+++ src/components/ContactSection/index.tsx
@@ -2,13 +2,17 @@ import { ContactForm } from '~components/index';
 
 function ContactSection() {
     return (
-        <section className="bg-background-950 pb-8" id="contact-me">
+        <section
+            className="bg-light-background-950 pb-8 7xl:py-12 11xl:py-16 dark:bg-dark-background-700"
+            id="contact-me"
+        >
             <div className="xs:py-6 lg:py-12">
                 {/* Contact Section */}
                 <h2 className="h2-text text-center text-light-text-900 dark:text-dark-text-800">
                     Contact Me
                 </h2>
-                <div className="form-ctr">
+
+                <div className="form-ctr xs:mt-0 lg:mt-2 2xl:mt-4 7xl:mt-6 11xl:mt-8">
                     <p className="body-text text-light-text-600 dark:text-dark-text-700">
                         Feel free to send me an email using the contact form
                         below, I will try to respond to you as soon as possible.
