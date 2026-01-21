4fd1f6016f41e0a83ad20f2156f5d4e631f344ed
Visakan Kirubakaran <72267229+vikiru@users.noreply.github.com>
2026-01-21 18:12:47 -0500

    refactor: re-organize component dir structure

---
+++ src/components/layout/Layout/index.tsx
@@ -0,0 +1,23 @@
+import NavBar from '@/components/layout/NavBar';
+import Footer from '@/components/sections/Footer';
+import { useMetaData } from '@/hooks/useMetaData';
+
+type LayoutProps = {
+  title: string;
+  description: string;
+  children: React.ReactNode;
+};
+
+function Layout({ title, description, children }: LayoutProps) {
+  useMetaData(title, description);
+
+  return (
+    <div className="scroller bg-light-background-950 dark:bg-dark-background-700">
+      <NavBar />
+      <main>{children}</main>
+      <Footer />
+    </div>
+  );
+}
+
+export default Layout;
