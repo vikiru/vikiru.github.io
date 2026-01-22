059f5a9eff8736d5f31743a3dc456df322e029c5
Visakan Kirubakaran <72267229+vikiru@users.noreply.github.com>
2026-01-21 19:06:54 -0500

    refactor: re-design courses page and existing components

--- src/routes/education/courses.tsx
+++ src/routes/education/courses.tsx
@@ -1,18 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router';
-import Layout from '@/components/layout/Layout';
-import CourseContainer from '@/components/sections/CourseContainer';
+import CoursesPage from '@/components/sections/Education/CoursesPage';
 
 export const Route = createFileRoute('/education/courses')({
   component: CoursesPage,
 });
-
-function CoursesPage() {
-  return (
-    <Layout
-      description="Explore the university courses I took during my degree at Carleton University."
-      title="Visakan Kirubakaran | Courses"
-    >
-      <CourseContainer />
-    </Layout>
-  );
-}
