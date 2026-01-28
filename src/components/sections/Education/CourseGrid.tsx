dde4b08bbcd656bb9d283a6c7672452e61cea488
Visakan Kirubakaran <72267229+vikiru@users.noreply.github.com>
2026-01-28 17:40:26 -0500

    style: improve responsiveness and styling on course page

--- src/components/sections/Education/CourseGrid.tsx
+++ src/components/sections/Education/CourseGrid.tsx
@@ -11,7 +11,7 @@ export function CourseGrid({ children, className }: CourseGridProps) {
     <section
       className={cn(
         'grid gap-4',
-        'grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3',
+        'grid-cols-1 lg:grid-cols-2 4xl:grid-cols-3',
         className,
       )}
     >
