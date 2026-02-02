de26b3030c5b17c723e83af0385b5dc3a934c435
Visakan Kirubakaran <72267229+vikiru@users.noreply.github.com>
2026-02-01 20:14:28 -0500

    chore: resolve lint error with shadcnui component

--- src/lib/components/ui/field.tsx
+++ src/lib/components/ui/field.tsx
@@ -211,7 +211,9 @@ function FieldError({
       <ul className="ml-4 flex list-disc flex-col gap-1">
         {uniqueErrors.map(
           (error, index) =>
-            error?.message && <li key={index}>{error.message}</li>,
+            error?.message && (
+              <li key={`${error.message}-${index}`}>{error.message}</li>
+            ),
         )}
       </ul>
     );
