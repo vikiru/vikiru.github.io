6aaa7b5c9593dfcce8609cc5678aa288e7c0b0cb
Visakan Kirubakaran <72267229+vikiru@users.noreply.github.com>
2026-02-05 19:30:07 -0500

    chore: change about section text to prev v3.3 text

--- src/components/sections/Home/About.tsx
+++ src/components/sections/Home/About.tsx
@@ -2,16 +2,13 @@ import { Card, CardContent } from '@/lib/components/ui/card';
 
 const aboutParagraphs = [
   {
-    text: 'My journey into the world of software development began with an introduction to computer science and programming. Following that, my software engineering degree at Carleton University provided me with a strong foundation of knowledge and exposure to various languages, including Racket, C, Java, Python, HTML, CSS, JavaScript, PHP, and SQL.',
+    text: 'My journey into the world of software development began with an introduction to computer science and programming, utilizing languages such as C++ and Java. Following that, my software engineering degree at Carleton University provided me with a strong foundation of knowledge and exposure to various languages, including Racket, C, Java, Python, HTML, CSS, JavaScript, PHP, and SQL.',
   },
   {
-    text: 'After graduating, my goal has been to continue learning and growing as a software developer. I actively seek out new and meaningful projects to challenge myself while contributing to meaningful projects.',
+    text: "After graduating, my goal has been to continue learning and growing as a software developer. I actively seek out new and meaningful projects to challenge myself, working with both familiar and unfamiliar technologies. My journey into web development has been particularly rewarding, and I've enjoyed working with technologies such as React, TailwindCSS, and Express.",
   },
   {
-    text: 'My journey into web development has been particularly rewarding, and I have enjoyed working with technologies such as React, TailwindCSS, and Express. I love creating software, no matter the platform. The process of refining a rough idea into something polished and functional through iterative development excites me.',
-  },
-  {
-    text: 'I am particularly drawn to projects where the end product is easily accessible to anyone, regardless of their device. I strive for inclusivity in technology, creating software that ensures a seamless experience for all users regardless of their abilities.',
+    text: 'I love creating software, no matter the platform. The process of refining a rough idea into something polished and functional through iterative development excites me. I am particularly drawn to projects where the end product is easily accessible to anyone, regardless of their device.',
   },
 ];
 
