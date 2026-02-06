a76d81004129625abd249c959926495b3b7fd000
Visakan Kirubakaran <72267229+vikiru@users.noreply.github.com>
2026-02-05 19:36:51 -0500

    chore: preload and preconnect to google fonts/stylesheet in head tag

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -48,6 +48,15 @@ export const Route = createRootRoute({
     links: [
       { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
       { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossOrigin: '' },
+      {
+        rel: 'preload',
+        as: 'style',
+        href: 'https://fonts.googleapis.com/css2?family=Cinzel:wght@400;700&family=Lato:ital,wght@0,400;0,700;1,400&family=Raleway:wght@400;600;700;900&display=swap',
+      },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Cinzel:wght@400;700&family=Lato:ital,wght@0,400;0,700;1,400&family=Raleway:wght@400;600;700;900&display=swap',
+      },
       { rel: 'stylesheet', href: indexCss },
       {
         rel: 'icon',
